ac59d0240ec122babcbf6780b5d4c4e73d489fb1
Igor Gritsenko <igor@gritsenko.biz>
2020-11-14 15:08:07 +0300

    Add project files.

---
+++ MyTestApp/MyTestApp.Droid/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using Android.Content;
+using Android.Views;
+
+namespace MyTestApp.Droid
+{
+	[Activity(
+			MainLauncher = true,
+			ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
+			WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden
+		)]
+
+    [IntentFilter(
+        new[] {
+            Android.Content.Intent.ActionView
+        },
+        Categories = new[] {
+            Android.Content.Intent.CategoryDefault,
+            Android.Content.Intent.CategoryBrowsable
+        },
+        DataMimeType = "*/*",
+        DataSchemes = new[] { "content", "file" },
+        DataPathPattern = ".*\\.png"
+        )]
+
+    public class MainActivity : Windows.UI.Xaml.ApplicationActivity
+	{
+	}
+}
+
